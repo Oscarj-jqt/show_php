b7211ebb89003fe713b4aef256079218963876d1
Aryles <aryles_77@icloud.com>
2025-10-29 17:16:01 +0100

    front home

---
+++ back/src/View/home.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8"><title>Accueil</title>
+</head>
+<body>
+  <h1>Bienvenue sur le site de spectacles</h1>
+  <nav>
+    <a href="/shows">Liste des spectacles</a> |
+    <a href="/profile">Mon profil</a>
+  </nav>
+</body>
+</html>
